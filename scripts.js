bf9b3c884f4a067f65701823b93e6c71e00180fb
Luiz <luizechaque@gmail.com>
2026-02-13 22:55:51 -0300

    remodelando o projeto, adicionando UI, criado o arquivo style.css

--- scripts.js
+++ scripts.js
@@ -1,57 +1,57 @@
+const container = document.querySelector('.MainContainer');
+const botoes = document.createElement('div');
+botoes.className = 'botoes';
+
+
+['rock', 'paper', 'scissors'].forEach(option => {
+    const btn = document.createElement('button');
+    btn.textContent = option;
+    btn.addEventListener('click', () => playGame(option));
+    botoes.append(btn);
+  });
+
+const resultado = document.createElement('div');
+resultado.className = 'resultado';
+const escolhas = document.createElement('p');
+const resultados = document.createElement('p');
+resultado.append(escolhas, resultados);
+container.append(botoes, resultado)
+
+//LOGICA DO JOGO
 let humanScore = 0
 let computerScore = 0
 
-
 const random = (min, max) => {
-   return Math.floor(Math.random() * (max - min) + min);
-     
+   return Math.floor(Math.random() * (max - min) + min);  
     
+}
+
+const getHumanChoice = (option) => {
+    return option.textContent
 }
 
 const getComputerChoice = () => {
-    let numbers = random(1, 4)
-    
-    if (numbers == 1){
-        numbers = 'rock'
-    } else if (numbers == 2){
-        numbers = 'paper'
-    } else {
-        numbers = 'scissors'
-    }
-    
-    return numbers
+    const options = ['rock', 'paper', 'scissors']
+    return options[random(0,3)];
 
 }
 
-const getHumanChoice = () => {
-    const choice = prompt('rock, paper or scissors? ')
-    return choice
+const choice = ['Pedra', 'Papel', 'Tesoura'];
+
+const playGame = (humanChoice) => {
+    let computerChoice = getComputerChoice();
+    if ((humanChoice == 'rock' && computerChoice == 'scissors') ||
+        (humanChoice == 'paper' && computerChoice == 'rock') ||
+        (humanChoice == 'scissors' && computerChoice == 'paper')){
+        humanScore++;
+    } if (humanChoice === computerChoice){
+     
+    }else {
+        computerScore++;
+    } 
+
+  escolhas.textContent = `You chose: ${humanChoice} | The Computer chose: ${computerChoice}`;
+  resultados.textContent = `Your score: ${humanScore} | Computer Score:  ${computerScore}`;
 }
 
 
-while (humanScore < 3 && computerScore < 3){
-    let choice = getHumanChoice()
-    let numbers = getComputerChoice()
-    if (choice == 'rock' && numbers == 'scissors'){
-        humanScore += 1;
-        console.log('Ponto para o jogador! A pontuação atual é de', humanScore, 'para o humano contra', computerScore, 'para a maquina!');
-        }
-    else if (choice == 'paper' && numbers == 'rock'){
-        humanScore += 1;
-        console.log('Ponto para o jogador! A pontuação atual é de', humanScore, 'para o humano contra', computerScore, 'para a maquina!');
-        }
-    else if (choice == 'scissors' && numbers == 'paper'){
-      humanScore += 1;
-        console.log('Ponto para o jogador! A pontuação atual é de', humanScore, 'para o humano contra', computerScore, 'para a maquina!');
-        }
-    else {
-        computerScore += 1;
-        console.log('Ponto para a maquina! A pontuação atual é de', humanScore, 'para o humano contra', computerScore, 'para a maquina!');
-    }
-} 
-
-if (humanScore == 3){
-    console.log('Fim de jogo! Você venceu.')
-} else {
-    console.log('Fim de jogo! A maquina venceu.')
-}
